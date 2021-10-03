48938af079e1a3a33fd28826976961eac14b7d5d
Dinhden <nguyenvandinh99qn@gmail.com>
2021-10-03 09:52:30 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐 Chào em! 🖐',
+    introDesc: `Anh có điều này muốn hỏi, Em hãy trả lời thật lòng nha`,
+    btnIntro: 'Dạ',
+    title: 'Anh biết Em thích ngáo đá phải không ? 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích đó nha :> ',
+    btnYes: 'Có, em thật sự thích ngáo đá 💖',
+    btnNo: '💔 Anh hâm à, khùng này ! 💔 ',
+    question:'Nói cho Anh biết đi vì sao Em thích ngáo đá 😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì lúc ngáo em thấy anh đẹp trai đó Ahiiiii 😊😊',
+    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you x3000 😘',
+    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    btnAccept: 'Ok anh yêu 💖',
+    messLink: 'https://www.facebook.com/profile.php?id=100015527053775'
+}
